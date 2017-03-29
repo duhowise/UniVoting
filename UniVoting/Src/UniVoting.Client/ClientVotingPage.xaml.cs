0da9fb08afe487f9f024e8192f514da2f1d95272
profause <profause@gmail.com>
2017-03-29 11:37:49 +0000

    added admin import voters page added client voting page added client vote completed page added candidate control

---
+++ UniVoting/Src/UniVoting.Client/ClientVotingPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UniVoting.Client
+{
+    /// <summary>
+    /// Interaction logic for ClientVotingPage.xaml
+    /// </summary>
+    public partial class ClientVotingPage : Page
+    {
+        public ClientVotingPage()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 10; i++)
+            {
+                this.candidatesHolder.Children.Add(new CandidateControl());
+            }
+        }
+
+
+    }
+}
--- UniVoting/Src/UniVoting.Client/MainWindow.xaml.cs
+++ UniVoting/Src/UniVoting.Client/MainWindow.xaml.cs
@@ -12,17 +12,19 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace UniVoting.Client
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            PageHolder.Content = new ClientVotingPage();
         }
     }
 }
