82e05a8ce997dc96a6cddd84eb207179b20b7b53
duho wise <wsduho>
2017-04-07 08:35:09 +0000

    Added Vote Report

---
+++ UniVoting/Src/Univoting.Reporting/VoteReport.cs
@@ -0,0 +1,27 @@
+namespace Univoting.Reporting
+{
+    using System;
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Windows.Forms;
+    using Telerik.Reporting;
+    using Telerik.Reporting.Drawing;
+
+    /// <summary>
+    /// Summary description for VoteReport.
+    /// </summary>
+    public partial class VoteReport : Telerik.Reporting.Report
+    {
+        public VoteReport()
+        {
+            //
+            // Required for telerik Reporting designer support
+            //
+            InitializeComponent();
+
+            //
+            // TODO: Add any constructor code after InitializeComponent call
+            //
+        }
+    }
+}
