c0efa0afdaec4e623188b9f3350bc241d8f8caca
duhowise <duhowise@gmail.com>
2018-03-04 23:30:12 +0000

    Rwfactored Indentations

--- UniVoting/Src/UniVoting.Data/Implementations/CandidateRepository.cs
+++ UniVoting/Src/UniVoting.Data/Implementations/CandidateRepository.cs
@@ -9,37 +9,5 @@ namespace UniVoting.Data.Implementations
 		{
 			
 		}
-		//public async Task<IEnumerable<Candidate>> GetAllCandidatesWithPositions()
-		//{
-		//	var candidates = new List<Candidate>();
-		//	using (var connection = new DbManager(connectionName))
-		//	{
-				
-		//		var data
-		//		foreach (var candidate in candidates)
-		//		{
-		//			//todo get positionid from position repository
-		//			candidate.Position = await;
-		//		}
-		//	}
-
-		//	return candidates;
-		//}
-
-
-		
-		//public async Task<Candidate>  Update(Candidate member)
-		//{
-		//	using (var connection = new DbManager(connectionName).Connection)
-		//	{
-		//		if (connection.State != ConnectionState.Open)
-		//		{
-		//			await connection.OpenAsync();
-
-		//		}
-		//		var id = Convert.ToInt32(await connection.UpdateAsync(member));
-		//		return await GetByIdAsync<Candidate>(id);
-		//	}
-		//}
 	}
 }
