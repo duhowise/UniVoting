c8d657f8f7f1fd95de6171f1ee5bd60500242114
duho wise <wduho@hotmail.com>
2025-09-14 07:55:19 +0000

    Move assembly metadata from AssemblyInfo.cs to csproj
    Assembly metadata attributes such as title, product, copyright, and version information were removed from AssemblyInfo.cs and added to the UniVoting.Data.csproj file. This modernizes the project by centralizing assembly information in the project file, aligning with .NET best practices for SDK-style projects.

--- UniVoting/Src/UniVoting.Data/Properties/AssemblyInfo.cs
+++ UniVoting/Src/UniVoting.Data/Properties/AssemblyInfo.cs
@@ -1,15 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("UniVoting.Data")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UniVoting.Data")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("84425ee4-65f7-43ff-80a3-02a0e1a067f6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
